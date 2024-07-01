e14042c0469a9fce9aaa366b9618d1a258d8f778
tanzeelxshafi <120320671+tanzeelxshafi@users.noreply.github.com>
2024-07-01 22:48:32 +0530

    Wtch

--- src/routes/watchList.js
+++ src/routes/watchList.js
@@ -5,15 +5,15 @@ import { watchList } from "../models/WatchList.js";
 
 router.get("/list", async (req, res) => {
     try{
-        const result = await fetch("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2024-06-26/2024-06-26?adjusted=true&sort=asc&apiKey=8Ksn44pycRJyFLxnm4fE6AAGqDXpwudK");
+        const result = await fetch("https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&include_otc=false&apiKey=8Ksn44pycRJyFLxnm4fE6AAGqDXpwudK");
         const data = await result.json();
-        const newData = new compStock({
-            ticker: data.ticker,
-            results: data.results
-        })
-        await newData.save();
-        console.log(newData);
-        res.status(200).send("Success!");  
+        // const newData = new compStock({
+        //     ticker: data.ticker,
+        //     results: data.results
+        // })
+        // await newData.save();
+        console.log(data);
+        res.status(200).send(data);  
     }catch(err){
         console.log(err);
         res.status(500).send("Error")
